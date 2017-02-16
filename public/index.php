01cd5064513de56c692e3c413ecd4b547f6bcb99
DXkite <DXkite@163.com>
2017-02-16 21:09:26 +0800

    console工具增强

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
 <?php    
     define('APP_DIR',__DIR__.'/../app');
-    define('SUDA_DIR',__DIR__.'/../system/');
     require_once SUDA_DIR.'/suda.php';
     
--- system/suda-console.php
+++ system/suda-console.php
@@ -1,5 +1,5 @@
 <?php
-define('APP_DIR',__DIR__.'/../app');
+defined('APP_DIR') or define('APP_DIR',__DIR__.'/../app');
 define('ROOT_PATH',dirname(__DIR__));
 require_once __DIR__.'/src/suda/core/System.php';
 suda\core\System::init();
