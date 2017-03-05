26ef0d01c8fb04b30bfd4bbc392b66aa00e6bd6a
DXkite <DXkite@163.com>
2017-03-05 14:52:05 +0800

    linux fix 02

--- system/suda-console.php
+++ system/suda-console.php
@@ -2,10 +2,8 @@
 defined('APP_DIR') or define('APP_DIR',__DIR__.'/../app');
 define('ROOT_PATH',dirname(__DIR__));
 require_once __DIR__.'/src/suda/core/Autoloader.php';  
-Autoloader::init();
-// 初始化包含路径
-Autoloader::addIncludePath(__DIR__.'/src');
-
+suda\core\Autoloader::init();
+suda\core\Autoloader::addIncludePath(__DIR__.'/src');
 suda\core\System::init();
 suda\core\Config::set('console',true);
 suda\core\ApplicationManager::getInstance()->console(APP_DIR);
--- system/suda.php
+++ system/suda.php
@@ -1,10 +1,6 @@
 <?php
-
 require_once __DIR__.'/src/suda/core/Autoloader.php';  
-Autoloader::init();
-// 初始化包含路径
-Autoloader::addIncludePath(__DIR__.'/src');
-
+suda\core\Autoloader::init();
+suda\core\Autoloader::addIncludePath(__DIR__.'/src');
 suda\core\System::init();
-// var_dump(Request::url());
 suda\core\ApplicationManager::getInstance()->run(APP_DIR);
