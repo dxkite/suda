f057fad7239194968eaf5060edc8b1d09db8a74c
DXkite <DXkite@163.com>
2017-03-08 21:21:57 +0800

    修改版本提示

--- system/__autoload.php
+++ system/__autoload.php
@@ -1,7 +1,7 @@
 <?php
 /* PHP版本检测 */
 if (version_compare(PHP_VERSION, '7.0.0', '<')) {
-    die('please use php 7.0.0 + to run this program!'."\r\n");
+    die('your php current vesion is '.PHP_VERSION.', please use 7.0.0 + to run this program!'."\r\n");
 }
 require_once __DIR__.'/src/suda/core/Autoloader.php';  
 suda\core\Autoloader::init();
