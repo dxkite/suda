4597402e26894511044b5d6c5b6ff93587ab8ac7
DXkite <DXkite@163.com>
2017-03-08 17:21:27 +0800

    添加系统版本检测

--- system/__autoload.php
+++ system/__autoload.php
@@ -1,4 +1,8 @@
 <?php
+/* PHP版本检测 */
+if (version_compare(PHP_VERSION, '7.0.0', '<')) {
+    die('please use php 7.0.0 + to run this program!'."\r\n");
+}
 require_once __DIR__.'/src/suda/core/Autoloader.php';  
 suda\core\Autoloader::init();
 suda\core\Autoloader::addIncludePath(__DIR__.'/src');
