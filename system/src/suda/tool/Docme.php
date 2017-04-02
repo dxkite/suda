777875f6d999c56849f2fb8549d4281d5063b418
DXkite <DXkite@163.com>
2017-04-02 16:53:31 +0800

    添加：DOC生成器【待完善】

---
+++ system/src/suda/tool/Docme.php
@@ -0,0 +1,58 @@
+<?php
+namespace suda\tool;
+
+/**
+* Doc Me
+* create a doc for class or function
+* use markdown style to doc this.
+*/
+class Docme
+{
+    protected $me=null;
+    protected $info=null;
+
+    public function __construct($that)
+    {
+        $this->me=$that;
+    }
+    public function setTemplate(string $template)
+    {
+    }
+    public function info()
+    {
+    }
+    public function export(string $path=null)
+    {
+    }
+    protected function parseDoc(string $docs)
+    {
+        return preg_replace('/^(\b+)?\/?\*\/?/m','',$docs);
+    }
+
+    protected function docClass($class)
+    {
+        $class=new ReflectionClass($class);
+        $methods=[];
+        foreach ($class->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
+            $name=$method->getShortName();
+            if (preg_match('/^action(.+?)$/', $name, $match)) {
+                $name=lcfirst($match[1]??$name);
+                $methods[$name]['doc']=$this->parseDoc($method->getDocComment());
+                foreach ($method->getParameters() as $param) {
+                    $methods[$name]['parameters'][$param->getName()]['pos']=$param->getPosition();
+                    if ($param->hasType()) {
+                        $methods[$name]['parameters'][$param->getName()]['type']=$param->getType()->__toString();
+                    }
+                    if ($param->isDefaultValueAvailable()) {
+                        $methods[$name]['parameters'][$param->getName()]['default']=$param->getDefaultValue();
+                    }
+                }
+            }
+        }
+        return ['doc'=>$this->parseDoc($class->getDocComment()),'methods'=>$methods];
+    }
+    public function __toString()
+    {
+        return $this->export();
+    }
+}
