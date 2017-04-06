d3e011d4192d9ef0483d5eb20e6cd0934519d08d
DXkite <DXkite@163.com>
2017-04-06 12:21:37 +0800

    修改：添加自己

--- system/__autoload.php
+++ system/__autoload.php
@@ -5,4 +5,3 @@ if (version_compare(PHP_VERSION, '7.0.0', '<')) {
 }
 require_once __DIR__.'/src/suda/core/Autoloader.php';  
 suda\core\Autoloader::init();
-suda\core\Autoloader::addIncludePath(__DIR__.'/src');
