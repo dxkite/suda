93e8ae6b8f0b1f17019b75fbc03cf211ddee0484
DXkite <dxkite@163.com>
2017-06-23 20:29:46 +0800

    删除:console工具

--- system/tool/run.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ .'/../suda-console.php';
-$get=getopt('m:c:');
-if (isset($get['m']) && isset($get['c'])){
-    Application::activeModule($get['m']);
-    (new \suda\tool\Command($get['c']))->exec();
-}else{
-    echo 'lack of arguments!';
-}
