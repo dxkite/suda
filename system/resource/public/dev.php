e8862e29945ee7a930c515ca32fb5a69dfced808
DXkite <dxkite@163.com>
2017-07-05 21:37:58 +0800

    代码控制

---
+++ system/resource/public/dev.php
@@ -0,0 +1,17 @@
+<?php #1498485376
+    
+    // 应用所在目录
+    define('APP_DIR', __DIR__.'/../app');
+    // 系统所在目录
+    define('SYSTEM',__DIR__.'/../system/');
+    // 网站根目录位置
+    define('APP_PUBLIC',__DIR__);
+    // 开发者模式
+    define('DEBUG',true);
+    // 日志纪录等级
+    define('LOG_LEVEL', 'info');
+    // 输出日志详细信息到json文档
+    define('LOG_JSON',true);
+    // 输出详细信息添加到日志末尾
+    define('LOG_FILE_APPEND',true);
+    require_once SYSTEM.'/suda.php';
---
+++ system/resource/public/index.php
@@ -0,0 +1,19 @@
+<?php #1497446012
+ 
+    // 应用所在目录
+    define('APP_DIR', __DIR__.'/../app');
+    // 系统所在目录
+    define('SYSTEM', __DIR__.'/../system/');
+    // 网站根目录位置
+    define('APP_PUBLIC', __DIR__);
+    // 关闭开发者模块
+    define('DISALLOW_MODULES', 'suda');
+    // 开发者关闭模式
+    define('DEBUG', false);
+    // 错误等级
+    define('LOG_LEVEL', 'trace');
+    // 输出日志详细信息到json文档
+    define('LOG_JSON',false);
+    // 输出详细信息添加到日志末尾
+    define('LOG_FILE_APPEND',false);
+    require_once SYSTEM.'/suda.php';
