5ce24d3e4713b7ce56acad90d24bf514a676346c
DXkite <dxkite@163.com>
2017-07-29 11:00:37 +0800

    发布：v1.2.7

--- system/src/suda/core/System.php
+++ system/src/suda/core/System.php
@@ -22,7 +22,7 @@ defined('SYSTEM_DIR') or define('SYSTEM_DIR', dirname(dirname(dirname(__DIR__)))
 defined('SYSTEM_RESOURCE') or define('SYSTEM_RESOURCE', SYSTEM_DIR.'/resource');
 defined('DEBUG') or define('DEBUG', false);
 defined('IS_LINUX') or define('IS_LINUX',DIRECTORY_SEPARATOR ===  '/');
-define('SUDA_VERSION', '1.2.6');
+define('SUDA_VERSION', '1.2.7');
 
 require_once __DIR__.'/Debug.php';
 require_once __DIR__.'/functions.php';
