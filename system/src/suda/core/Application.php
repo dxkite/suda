3a96b13792a7737878950bef133377c0ba83e256
DXkite <dxkite@163.com>
2017-09-10 23:25:34 +0800

    优化：应用代码逻辑

--- system/src/suda/core/Application.php
+++ system/src/suda/core/Application.php
@@ -22,18 +22,25 @@ use suda\exception\ApplicationException;
 
 class Application
 {
-    protected $path;
-    protected static $active_module;
-    protected static $module_dirs=null;
-    protected static $module_cache;
-    protected static $module_live=null;
-    protected static $module_configs=null;
-    
+    // app 目录
+    private $path;
+    // 当前模块名
+    private static $active_module;
+    // 激活的模块
+    private static $module_live=null;
+    // 模块配置
+    private static $module_configs=null;
+    // 模块名缓存
+    private static $module_name_cache=[];
+    // 模块目录装换成模块名
+    private static $module_dir_name=[];
+
     public function __construct(string $app)
     {
         _D()->trace(__('application load %s', $app));
         $this->path=$app;
-        // 基本常量
+
+        // 注册基本常量
         defined('MODULES_DIR') or define('MODULES_DIR', Storage::path(APP_DIR.'/modules'));
         defined('RESOURCE_DIR') or define('RESOURCE_DIR', Storage::path(APP_DIR.'/resource'));
         defined('DATA_DIR') or define('DATA_DIR', Storage::path(APP_DIR.'/data'));
@@ -69,29 +76,10 @@ class Application
         Autoloader::setNamespace(Config::get('app.namespace'));
         // 系统共享库
         Autoloader::addIncludePath(SHRAE_DIR);
-        // 加载模块主配置
-        self::loadAllModuleManifast();
-        // 解析模块
-        self::moduleMap();
-        // 模块共享库
-        $module_dirs=self::getModuleDirs();
-        // 激活模块
-        $module_use=self::getLiveModules();
-        // 安装 启用 活动
-        foreach ($module_dirs as $module_dir) {
-            if (Storage::isDir(MODULES_DIR.'/'.$module_dir.'/share')) {
-                Autoloader::addIncludePath(MODULES_DIR.'/'.$module_dir.'/share');
-            }
-            
-            if (in_array(self::moduleName($module_dir), $module_use)) {
-                // 加载监听器
-                if (Storage::exist($path=MODULES_DIR.'/'.$module_dir.'/resource/config/listener.json')) {
-                    Hook::loadJson($path);
-                }
-                // 设置语言包库
-                Locale::path(MODULES_DIR.'/'.$module_dir.'/resource/locales/');
-            }
-        }
+        // 读取目录，注册所有模块
+        self::registerModules();
+        // 加载模块
+        self::loadModules();
         // 调整模板
         Manager::theme(conf('app.template', 'default'));
         Hook::exec('Application:init');
@@ -106,21 +94,50 @@ class Application
         }
     }
 
+    /**
+     * 载入模块
+     *
+     * @return void
+     */
+    public static function loadModules()
+    {
+        // 模块共享库
+        $module_all=self::getModules();
+        // 激活模块
+        $module_use=self::getLiveModules();
+        // 安装 启用 活动
+        foreach ($module_all as $module_temp) {
+            $root=self::getModulePath($module_temp);
+            // 注册共享目录
+            if (Storage::isDir($share_path=$root.'/share')) {
+                Autoloader::addIncludePath($share_path);
+            }
+            // 是否激活
+            $is_live_module=in_array($module_temp, $module_use);
+            if ($is_live_module) {
+                // 加载监听器
+                if (Storage::exist($listener_path=$root.'/resource/config/listener.json')) {
+                    Hook::loadJson($listener_path);
+                }
+                // 设置语言包库
+                Locale::path($root.'/resource/locales/');
+            }
+        }
+    }
+
+    /**
+     * 获取所有模块
+     *
+     * @return void
+     */
     public static function getModules()
     {
-        if (is_null(self::$module_dirs)) {
-            // 解析模块
-            self::moduleMap();
-        }
-        return array_keys(self::$module_dirs);
+        return array_values(self::$module_dir_name);
     }
+
     public static function getModuleDirs()
     {
-        if (is_null(self::$module_dirs)) {
-            // 解析模块
-            self::moduleMap();
-        }
-        return array_values(self::$module_dirs);
+        return array_keys(self::$module_dir_name);
     }
 
     
@@ -128,7 +145,6 @@ class Application
     {
         return self::$active_module;
     }
-
 
     public static function getModuleConfig(string $module)
     {
@@ -163,20 +179,25 @@ class Application
         _D()->trace('exclude', json_encode($exclude));
         foreach ($modules as $index => $name) {
             $fullname=Application::getModuleFullName($name);
-            // _D()->notice($name, 'fullname> ['.$fullname.'] name in  array exclude> ['.in_array($name, $exclude).'] fullname in  array exclude> ['.in_array($fullname, $exclude).'] exist['.self::checkModuleExist($name).']');
+            // 剔除模块名
             if (!self::checkModuleExist($name) || in_array($fullname, $exclude)) {
-                // _D()->notice('exclude',$exclude);
                 unset($modules[$index]);
             } else {
                 $modules[$index]=$fullname;
             }
         }
+        // 排序，保证为数组
         sort($modules);
         _D()->trace('live modules', json_encode($modules));
         return self::$module_live=$modules;
     }
 
-    // 激活模块
+    /**
+     * 激活运行的模块
+     *
+     * @param string $module
+     * @return void
+     */
     public static function activeModule(string $module)
     {
         Hook::exec('Application:active', [$module]);
@@ -216,98 +237,87 @@ class Application
         return false;
     }
 
+    /**
+     * 获取模块名，不包含版本号
+     *
+     * @param string $name 不完整模块名
+     * @return void
+     */
     public static function getModuleName(string $name)
     {
         $name=self::getModuleFullName($name);
         return preg_replace('/:.+$/', '', $name);
     }
     
+    /**
+     * 获取模块全名（包括版本）
+     * name:version,name,namespace/name => namespace/name:version
+     * 未指定版本则调整到最优先版本
+     *
+     * @param string $name 不完整模块名
+     * @return void
+     */
     public static function getModuleFullName(string $name)
     {
-        return self::moduleName(self::getModuleDir($name));
-    }
-
-    /**
-    * 从模块名调整到模块文件夹
-    */
-    public static function getModuleDir(string $name)
-    {
-        // 历史记录不存在
-        if (is_null(self::$module_dirs)) {
-            // 解析模块
-            self::moduleMap();
+        // 存在缓存则返回缓存
+        if (isset(self::$module_name_cache[$name])) {
+            return self::$module_name_cache[$name];
         }
-        // 存在则从缓存调用
-        if (isset(self::$module_cache[$name])) {
-            return self::$module_cache[$name];
-        }
-        // 全部匹配
-        if (isset(self::$module_dirs[$name])) {
-            return self::$module_dirs[$name];
-        }
-        // MODULE_NAME_PREG
-        // 缩略匹配
         preg_match('/^(?:([a-zA-Z0-9_-]+)\/)?([a-zA-Z0-9_-]+)(?::(.+))?$/', $name, $matchname);
-
-        _D()->debug('match name', (isset($matchname[1])&&$matchname[1]?$matchname[1]:'(\w+\/)?'));
-
         $preg='/^'.(isset($matchname[1])&&$matchname[1]? preg_quote($matchname[1]).'\/':'(\w+\/)?') // 限制域
-        .preg_quote($matchname[2]). // 名称
-        (isset($matchname[3])&&$matchname[3]?':'.preg_quote($matchname[3]):'(:.+)?').'$/'; // 版本号
+            .preg_quote($matchname[2]) // 名称
+            .(isset($matchname[3])&&$matchname[3]?':'.preg_quote($matchname[3]):'(:.+)?').'$/'; // 版本号
         $targets=[];
-        _D()->debug($matchname, $preg);
-
-        foreach (self::$module_dirs as $modulename=>$moduledir) {
-            if (preg_match($preg, $modulename)) {
-                preg_match('/^(?:(\w+)\/)?(\w+)(?::(.+))?$/', $modulename, $matchname);
+        // _D()->debug($matchname, $preg);
+        // 匹配模块名，查找符合格式的模块
+        foreach (self::$module_configs as $module_name=>$module_config) {
+            // 匹配到模块名
+            if (preg_match($preg, $module_name)) {
+                preg_match('/^(?:(\w+)\/)?(\w+)(?::(.+))?$/', $module_name, $matchname);
                 // 获取版本号
                 if (isset($matchname[3])&&$matchname[3]) {
-                    $targets[$matchname[3]]=$moduledir;
+                    $targets[$matchname[3]]=$module_name;
                 } else {
-                    $targets[]=$moduledir;
+                    $targets[]=$module_name;
                 }
             }
         }
         // 排序版本
         uksort($targets, 'version_compare');
-        _D()->debug($targets);
-        // 获取最新版本
-        $dir=self::$module_cache[$name]=count($targets)>0?array_pop($targets):$name;
-        _D()->trace($name.' : '.$dir);
-        return $dir;
+        return count($targets)>0?array_pop($targets):$name;
     }
 
-    public static function moduleName(string $name)
+    /**
+     * 获取模块所在的文件夹名
+     *
+     * @param string $name
+     * @return void
+     */
+    public static function getModuleDir(string $name)
     {
-        return array_search($name, self::$module_dirs)?:$name;
-    }
-
-    public static function moduleMap()
-    {
-        if (!DEBUG && Storage::exist($path=TEMP_DIR.'/module-dir.php')) {
-            _D()->trace(__('load modules from %s', $path));
-            self::$module_dirs=require $path;
-        } else {
-            _D()->trace('refersh module map');
-            self::$module_dirs=self::refreshMap();
+        $name=self::getModuleFullName($name);
+        if (isset(self::$module_configs[$name])) {
+            return self::$module_configs[$name]['directory'];
         }
-        _D()->debug('module_dirs', self::$module_dirs);
     }
 
-    protected static function refreshMap()
+    /**
+     * 根据模块目录名转换成模块名
+     *
+     * @param string $dirname
+     * @return void
+     */
+    public static function moduleName(string $dirname)
     {
-        $modulemap=[];
-        _D()->debug('module config', self::$module_configs);
-        if (is_array(self::$module_configs)) {
-            foreach (self::$module_configs as $name => $info) {
-                $modulemap[$name]=$info['directory'];
-            }
-        }
-        ArrayHelper::export(TEMP_DIR.'/module-dir.php', '_module_map', $modulemap);
-        return $modulemap;
+        return self::$module_dir_name[$dirname]?:$name;
     }
 
-    public static function loadAllModuleManifast()
+    /**
+     * 注册所有模块信息
+     *
+     * @return void
+     */
+    private static function registerModules()
     {
         $dirs=Storage::readDirs(MODULES_DIR);
         foreach ($dirs as $dir) {
@@ -316,8 +326,10 @@ class Application
                 $json=Json::parseFile($file);
                 $name=$json['name'] ?? $dir;
                 $json['directory']=$dir;
+                $json['path']=MODULES_DIR.'/'.$dir;
                 $name.=isset($json['version'])?':'.$json['version']:'';
                 self::$module_configs[$name]=$json;
+                self::$module_dir_name[$dir]=$name;
             }
         }
     }
@@ -327,11 +339,12 @@ class Application
         return self::$module_configs;
     }
 
-    public static function getModulePath(string $module){
+    public static function getModulePath(string $module)
+    {
         return MODULES_DIR.'/'. self::getModuleDir($module);
     }
 
-    public static function configDBify()
+    private static function configDBify()
     {
         if (file_exists($path=RUNTIME_DIR.'/database.config.php')) {
             $config=include $path;
