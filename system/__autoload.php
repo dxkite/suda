4a12c01a57d116b999cabf109a0de6146186c325
DXkite <dxkite@163.com>
2017-10-02 21:05:09 +0800

    修改：控制台脚本引用

--- system/__autoload.php
+++ system/__autoload.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Suda FrameWork
+ *
+ * An open source application development framework for PHP 7.0.0 or newer
+ * 
+ * Copyright (c)  2017 DXkite
+ *
+ * @category   PHP FrameWork
+ * @package    Suda
+ * @copyright  Copyright (c) DXkite
+ * @license    MIT
+ * @link       https://github.com/DXkite/suda
+ * @version    since 1.2.9
+ */
+
 /* PHP版本检测 */
 if (version_compare(PHP_VERSION, '7.0.0', '<')) {
     die('your current  php vesion is '.PHP_VERSION.', <span style="color:red">please use 7.0.0 + to run this program!</span>'."\r\n");
