2df28ab8e169043b062fa7d9ffa66dfcba432469
DXkite <dxkite@163.com>
2017-10-23 19:36:43 +0800

    v1.2.10-release-88cd868

--- system/src/suda/tool/ZipHelper.php
+++ system/src/suda/tool/ZipHelper.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Suda FrameWork
+ *
+ * An open source application development framework for PHP 7.0.0 or newer
+ * 
+ * Copyright (c)  2017 DXkite
+ *
+ * @category   PHP FrameWork
+ * @package    Suda
+ * @copyright  Copyright (c) DXkite
+ * @license    MIT
+ * @link       https://github.com/DXkite/suda
+ * @version    since 1.2.10
+ */
+
 namespace suda\tool;
 
 use ZipArchive;
