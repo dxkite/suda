1747358000005e2de1d91e7afd8b3a465219ecc1
DXkite <dxkite@163.com>
2017-11-05 21:09:47 +0800

    优化：路由缓存

--- system/src/suda/core/Router.php
+++ system/src/suda/core/Router.php
@@ -18,21 +18,19 @@ namespace suda\core;
 use suda\tool\Command;
 use suda\tool\Json;
 use suda\tool\ArrayHelper;
+use suda\core\route\Mapping;
 
 // TODO:路由强化
 // TODO:路由模块化（添加命名空间）
 
 class Router
 {
-    protected $mapper;
-    protected $matchs=[];
-    protected $types=[];
     protected static $urltype=['int'=>'\d+','string'=>'[^\/]+','url'=>'.+'];
     protected static $router=null;
     protected $routers=[];
-
     protected static $cacheName=null;
     protected static $cacheModules=null;
+    const CACHE_NAME='route.mapping';
 
     private function __construct()
     {
@@ -70,57 +68,41 @@ class Router
         $simple_routers=[];
         $admin_routers=[];
         $module_path=Application::getModulePath($module);
-        debug()->trace(__('load module:%s [%s] path:%s', $module, Application::getModuleFullName($module), $module_path));
-        list($admin_prefix, $prefix)=self::getModulePrefix($module);
-        $module=Application::getModuleFullName($module);
-        $prefix_it= function (&$router, $key, $prefixinfo) use ($module) {
-            $prefix=$prefixinfo[0]?conf('app.admin', '/admin'):'/';
-            if (!(isset($router['anti-prefix']) && $router['anti-prefix'])) {
-                $prefix.=$prefixinfo[1];
-            }
-            $router['visit']='/'.trim($prefix.$router['visit'], '/');
-            $router['role']=$prefixinfo[0]?'admin':'simple';
-            $router['module']=$module;
-        };
+        debug()->trace(__('load module:%s path:%s', $module, $module_path));
         // 加载前台路由
         if (Storage::exist($file=$module_path.'/resource/config/router.json')) {
-            $simple_routers= self::loadModuleJson($module, $file);
+            $simple_routers= self::loadModuleJson(Mapping::ROLE_SIMPLE,$module, $file);
             debug()->trace(__('loading simple route from file %s', $file));
-            array_walk($simple_routers, $prefix_it, [false,$prefix]);
         }
         // 加载后台路由
         if (Storage::exist($file=$module_path.'/resource/config/router_admin.json')) {
-            $admin_routers= self::loadModuleJson($module, $file);
+            $admin_routers= self::loadModuleJson(Mapping::ROLE_ADMIN,$module, $file);
             debug()->trace(__('loading admin route from file  %s', $file));
-            array_walk($admin_routers, $prefix_it, [true,$admin_prefix]);
         }
-       
         $this->routers=array_merge($this->routers, $admin_routers, $simple_routers);
     }
 
-    protected function loadModuleJson(string $module, string $jsonfile)
+    protected function loadModuleJson(int $role,string $module, string $jsonfile)
     {
         $routers=Json::loadFile($jsonfile);
         $router=[];
         foreach ($routers as $name => $value) {
-            $router[$module.':'.$name]=$value;
+            $mapping=Mapping::createFromRouteArray($role,$module,$name,$value);
+            $router[$mapping->getFullName()]=$mapping;
         }
         return $router;
     }
 
     protected function loadFile()
     {
-        $this->routers=require self::cacheFile('router.cache.php');
-        $this->types=require self::cacheFile('types.cache.php');
-        $this->matchs=require self::cacheFile('matchs.cache.php');
+        $routers=storage()->get(self::cacheFile(self::CACHE_NAME));
+        $this->routers=unserialize($routers);
     }
 
     protected function saveFile()
     {
-        ArrayHelper::export(self::cacheFile('router.cache.php'), '_router', $this->routers);
-        ArrayHelper::export(self::cacheFile('types.cache.php'), '_types', $this->types);
-        ArrayHelper::export(self::cacheFile('matchs.cache.php'), '_matchs', $this->matchs);
         storage()->put(self::cacheFile('.modules'),implode("\r\n", self::$cacheModules));
+        storage()->put(self::cacheFile(self::CACHE_NAME),serialize($this->routers));
     }
 
     protected function loadModulesRouter()
@@ -129,23 +111,18 @@ class Router
         if (conf('debug', false)) {
             self::prepareRouterInfo();
         } else {
-            if (!self::routerCached()) {
+            if (self::routerCached()) {
+                self::loadFile();
+            }else{
                 self::prepareRouterInfo();
             }
-            self::loadFile();
         }
     }
 
     
     public function routerCached()
     {
-        if (!file_exists(self::cacheFile('router.cache.php'))) {
-            return false;
-        }
-        if (!file_exists(self::cacheFile('types.cache.php'))) {
-            return false;
-        }
-        if (!file_exists(self::cacheFile('matchs.cache.php'))) {
+        if (!file_exists(self::cacheFile(self::CACHE_NAME))) {
             return false;
         }
     }
@@ -156,95 +133,21 @@ class Router
         foreach ($modules as $module) {
             self::load($module);
         }
-        self::buildRouterMap();
         Hook::exec('Router:prepareRouterInfo', [$this]);
         // 缓存路由信息
         self::saveFile();
     }
 
-    public function watch(string $name, string $url)
-    {
-        $this->matchs[$name]=self::buildMatch($name, $url);
-    }
-
     protected function matchRouterMap()
     {
         $request=Request::getInstance();
-        foreach ($this->matchs as $name=>$preg) {
-            $matchExp=conf('app.url.ignoreCase',true)?'/^'.$preg.'$/i':'/^'.$preg.'$/';
-            // debug()->d('url:'.$request->url().'; preg:'.'/^'.$preg.'$/');
-            if (preg_match($matchExp, $request->url(), $match)) {
-                // 检验方法
-                if (isset($this->routers[$name]['method']) && count($this->routers[$name]['method'])>0) {
-                    // 调整方法大小
-                    array_walk($this->routers[$name]['method'], function ($value) {
-                        return strtoupper($value);
-                    });
-                    // 方法不匹配
-                    if (!in_array(strtoupper($request->method()), $this->routers[$name]['method'])) {
-                        continue;
-                    }
-                }
-                // URL禁用
-                if ($this->routers[$name]['hidden']??false) {
-                    continue;
-                }
-                // 检验接口参数
-                array_shift($match);
-                if (count($match)>0) {
-                    foreach ($this->types[$name] as $param_name =>$type) {
-                        $value=array_shift($match);
-                        if ($type==='int') {
-                            $value=intval($value);
-                        } else {
-                            $value=urldecode($value);
-                        }
-                        // 填充$_GET
-                        $_GET[$param_name]=$value;
-                        $request->set($param_name, $value);
-                    }
-                }
-                // 自定义过滤
-                if (!Hook::execIf('Router:filter', [$name,$this->routers[$name]], false)) {
-                    continue;
-                }
-                return $name;
+        $ignoreCase=conf('app.url.ignoreCase',true);
+        foreach ($this->routers as $name => $mapping ){
+            if($mapping->match($request,$ignoreCase)){
+                return $mapping;
             }
         }
         return false;
-    }
-
-    protected function buildRouterMap()
-    {
-        foreach ($this->routers as $name => $router) {
-            self::watch($name, $router['visit']);
-        }
-    }
-
-
-    protected function buildMatch(string $name, string $url)
-    {
-        $types=&$this->types;
-        $urltype=self::$urltype;
-        // 转义字符
-        $url=preg_replace('/([\/\.\\\\\+\*\(\^\)\?\$\!\<\>\-])/', '\\\\$1', $url);
-        // 添加忽略
-        $url=preg_replace('/\[(\S+)\]/', '(?:$1)?', $url);
-        // 编译页面参数
-        $url=preg_replace_callback('/\{(?:(\w+)(?::(\w+))?)(?:=(\w+))?\}([?])?/', function ($match) use ($name, &$types, $urltype) {
-            // debug()->debug($match);
-            $size=isset($types[$name])?count($types[$name]):0;
-            $param_name=$match[1]!==''?$match[1]:$size;
-            $param_type=  $match[2] ?? 'string';
-            $ignore=isset($match[4])?'?':'';
-            $types[$name][$param_name]=$param_type;
-            if (isset($urltype[$param_type])) {
-                return '('.$urltype[$param_type].')'.$ignore;
-            } else {
-                return '(.+)'.$ignore;
-            }
-        }, $url);
-        return $url;
     }
     
     /**
@@ -289,17 +192,20 @@ class Router
         list($module, $name)=self::parseName($name);
         $module=Application::getModuleFullName($module);
         $name=$module.':'.$name;
-        if (isset($this->types[$name])) {
-            $keys=array_keys($this->types[$name]);
-            $values=[];
-            foreach ($keys as $key) {
-                if (count($args)) {
-                    $values[$key]=array_shift($args);
-                } else {
-                    break;
+        if (isset($this->routers[$name])){
+            $types=$this->routers[$name]->getTypes();
+            if ($types) {
+                $keys=array_keys($types);
+                $values=[];
+                foreach ($keys as $key) {
+                    if (count($args)) {
+                        $values[$key]=array_shift($args);
+                    } else {
+                        break;
+                    }
                 }
+                return $values;
             }
-            return $values;
         }
         return [];
     }
@@ -309,49 +215,23 @@ class Router
         list($module, $name)=self::parseName($name);
         $module=Application::getModuleFullName($module);
         $name=$module.':'.$name;
-        // debug()->debug($name);
-        $url= '';
         if (isset($this->routers[$name])) {
-            // 路由存在
-            $url.=preg_replace('/[?|]/', '\\\1', $this->routers[$name]['visit']);
-            $url=preg_replace_callback('/\{(?:(\w+)(?::(\w+))?)(?:=(\w+))?\}/', function ($match) use ($name, & $values) {
-                $param_name=$match[1];
-                $param_type= $match[2] ?? 'url';
-                $param_default=$match[3]??'';
-                if (isset($values[$param_name])) {
-                    if ($param_type==='int') {
-                        $val= intval($values[$param_name]);
-                    }
-                    $val=$values[$param_name];
-                    unset($values[$param_name]);
-                    return $val;
-                } else {
-                    return $param_default;
-                }
-            }, preg_replace('/\[(.+?)\]/', '$1', $url));
+             return $this->routers[$name]->createUrl($values);
         } else {
             debug()->warning(__('get url for %s failed,module:%s args:%s', $name, $module, json_encode($values)));
             return '#the-router-['.$name.']-is-undefined--please-check-out-router-list';
         }
-        if (count($values)) {
-            return Request::getInstance()->baseUrl(). trim($url, '/').'?'.http_build_query($values, 'v', '&', PHP_QUERY_RFC3986);
-        }
-        return Request::getInstance()->baseUrl(). trim($url, '/');
     }
-
 
     public function dispatch()
     {
         debug()->time('dispatch');
-        self::buildRouterMap();
-        // Hook前置路由（自定义过滤器|自定义路由）
         if (Hook::execIf('Router:dispatch::before', [Request::getInstance()], true)) {
-            if (($router_name=self::matchRouterMap())!==false) {
-                // debug()->debug('dispatch match '.$router_name);
+            if (($mapping=self::matchRouterMap())!==false) {
                 debug()->timeEnd('dispatch');
-                Response::setName($router_name);
+                Response::setName($mapping->getFullName());
                 debug()->time('run router');
-                self::runRouter($this->routers[$router_name]);
+                $this->runRouter($mapping);
                 debug()->timeEnd('run router');
             } else {
                 Hook::exec('system:404');
@@ -371,9 +251,7 @@ class Router
     {
         $name=self::getRouterFullName($name);
         if (isset($this->routers[$name])) {
-            $router= $this->routers[$name];
-            $router['match']=$this->matchs[$name];
-            return $router;
+            return $this->routers[$name];
         }
     }
     
@@ -389,8 +267,7 @@ class Router
         $name=self::getRouterFullName($name);
         $alias=self::getRouterFullName($alias);
         if (isset($this->routers[$name])) {
-            $this->routers[$alias]=$this->routers[$alias]??$this->routers[$name];
-            $this->matchs[$alias]=$this->matchs[$alias]??$this->matchs[$name];
+            $this->routers[$alias]=$this->routers[$name];
         }
     }
     
@@ -407,9 +284,7 @@ class Router
         $alias=self::getRouterFullName($alias);
         if (isset($this->routers[$name])) {
             if (isset($this->routers[$alias])) {
-                $this->routers[$name]['class']=$this->routers[$alias]['class'];
-                $this->routers[$name]['method']=$this->routers[$alias]['method']??[];
-                $this->routers[$name]['module']=$this->routers[$alias]['module'];
+                $this->routers[$name]=$this->routers[$alias];
             }
         }
     }
@@ -423,16 +298,9 @@ class Router
      */
     public function routerMove(string $name, string $alias)
     {
-        $name=self::getRouterFullName($name);
+        $this->routerReplace($name,$alias);
         $alias=self::getRouterFullName($alias);
-        if (isset($this->routers[$name])) {
-            if (isset($this->routers[$alias])) {
-                $this->routers[$name]['class']=$this->routers[$alias]['class'];
-                $this->routers[$name]['method']=$this->routers[$alias]['method']??[];
-                $this->routers[$name]['module']=$this->routers[$alias]['module'];
-                unset($this->router[$alias]);
-            }
-        }
+        unset($this->router[$alias]);
     }
     
 
@@ -446,16 +314,14 @@ class Router
      * @param array $method
      * @return void
      */
-    public function addRouter(string $name, string $url, string $class, string $module, array $method=[])
+    public function addRouter(string $name, string $url, string $class, string $module, array $method=[],bool $autoPrefix=false)
     {
-        $module=Application::getModuleFullName($module);
-        $name=$module.':'.$name;
-        $this->routers[$name]['class']=$class;
-        $this->routers[$name]['method']=$method;
-        $this->routers[$name]['module']=$module;
-        $this->routers[$name]['visit']=$url;
-        self::watch($name, $url);
-        return $name;
+        $mapping=new Mapping($name,$url,$class.'->onRequest',$module,$method);
+        $fillName=$mapping->getFullName();
+        $this->routers[$fillName]=$mapping;
+        $mapping->setAntiPrefix(!$autoPrefix);
+        $mapping->build();
+        return $fillName;
     }
 
     /**
@@ -469,11 +335,11 @@ class Router
     public function replaceMatch(string $name, string $url, bool $preg=false)
     {
         $name=self::getRouterFullName($name);
-        if (isset($this->matchs[$name])) {
+        if (isset($this->routers[$name])) {
             if ($preg) {
-                return $this->matchs[$name]=$url;
+                return $this->routers[$name]->setMapping($url);
             }
-            return $this->matchs[$name]=self::buildMatch($name, $url);
+            return $this->routers[$name]->setUrl($url)->build();
         }
     }
 
@@ -489,32 +355,31 @@ class Router
     {
         $name=self::getRouterFullName($name);
         if (isset($this->routers[$name])) {
-            $router= $this->routers[$name];
-            $router['class']= $class;
-            if ($method) {
-                $router['method']=$method;
+            $this->routers[$name]->setCallback($class.'->onRequest');
+            if ($module){
+                $this->routers[$name]->setModule($module);
             }
-            if ($module) {
-                $router['module']=Application::getModuleFullName($module);
+            if ($method){
+                $this->routers[$name]->setMethod($method);
             }
-            return $this->routers[$name]=$router;
+            return $this->routers[$name];
         }
     }
 
-    protected static function runRouter(array $router)
+    protected static function runRouter(Mapping $mapping)
     {
         // 全局钩子:重置Hook指向
-        Hook::exec('Router:runRouter::before', [&$router]);
+        Hook::exec('Router:runRouter::before', [&$mapping]);
         // debug()->time('active Module');
         // 激活模块
-        (new Command(System::getAppClassName().'::activeModule'))->exec([$router['module']]);
+        (new Command(System::getAppClassName().'::activeModule'))->exec([$mapping->getModule()]);
         // debug()->timeEnd('active Module');
         debug()->time('request');
         // 运行请求
-        (new Command($router['class'].'->onRequest'))->exec([Request::getInstance()]);
+        $mapping->run();
         debug()->timeEnd('request');
         // 请求结束
-        Hook::exec('Router:runRouter::after', [&$router]);
+        Hook::exec('Router:runRouter::after', [&$mapping]);
     }
 
     public static function error404()
@@ -542,7 +407,6 @@ class Router
             self::$cacheModules=$module_use;
             self::$cacheName=substr(md5(implode('-', $module_use)), 0, 8);
         }
-        
         $path=CACHE_DIR.'/router/'.self::$cacheName;
         Storage::path($path);
         return $path.'/'.$name;
---
+++ system/src/suda/core/route/Mapping.php
@@ -0,0 +1,218 @@
+<?php
+namespace suda\core\route;
+
+use suda\tool\Command;
+use suda\core\Request;
+
+class Mapping
+{
+    protected $method=[];
+    protected $url;
+    protected $mapping;
+    protected $callback;
+    protected $module;
+    protected $name;
+    protected $role;
+    protected $types;
+
+    protected $antiPrefix=false;
+    protected $hidden=false;
+
+    const ROLE_ADMIN=0;
+    const ROLE_SIMPLE=1;
+
+    protected static $urlType=['int'=>'\d+','string'=>'[^\/]+','url'=>'.+'];
+
+    
+    public function __construct(string $name, string $url, string $callback, string $module, array $method=[], int $role=self::ROLE_SIMPLE)
+    {
+        $this->module=app()->getModuleFullName($module);
+        $this->name=$name;
+        array_walk($method, function ($value) {
+            return strtoupper($value);
+        });
+        $this->method= $method;
+        $this->callback=$callback;
+        $this->role=$role;
+        $this->url=$url;
+    }
+
+    public function getFullName()
+    {
+        return $this->module.':'.$this->name;
+    }
+
+    public function match(Request $request, bool $ignoreCase=true)
+    {
+        $matchExp=$ignoreCase?'/^'.$this->mapping.'$/i':'/^'.$this->mapping.'$/';
+        if (preg_match($matchExp, $request->url(), $match)) {
+            // 方法不匹配
+            if (count($this->method)>0 && !in_array(strtoupper($request->method()), $this->method)) {
+                return false;
+            }
+            if ($this->hidden) {
+                return false;
+            }
+            // 检验接口参数
+            array_shift($match);
+            if (count($match)>0) {
+                foreach ($this->types as $paramName =>$type) {
+                    $value=array_shift($match);
+                    if ($type==='int') {
+                        $value=intval($value);
+                    } else {
+                        $value=urldecode($value);
+                    }
+                    // 填充$_GET
+                    $_GET[$paramName]=$value;
+                    $request->set($paramName, $value);
+                }
+            }
+            // 自定义过滤
+            if (!hook()->execIf('Router:filter', [$this->getFullName(),$this], false)) {
+                return false;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function run()
+    {
+        return (new Command($this->callback))->exec([Request::getInstance()]);
+    }
+
+    public function build()
+    {
+        $urlMapping='/'.trim($this->url, '/');
+        if (!$this->antiPrefix) {
+            $urlMapping='/'.trim($this->getPrefix().$urlMapping, '/');
+        }
+        $this->mapping=$this->buildMatch($urlMapping);
+        return $this;
+    }
+
+    public function setCallback(string $callback)
+    {
+        $this->callback=$callback;
+        return $this;
+    }
+
+    public function setModule(string $module)
+    {
+        $this->module=$module;
+        return $this;
+    }
+    
+    public function setMethod(string $method)
+    {
+        $this->method=$method;
+        return $this;
+    }
+
+    public function createUrl(array $args)
+    {
+        $url=preg_replace('/[?|]/', '\\\1', $this->url);
+        $url=preg_replace_callback('/\{(?:(\w+)(?::(\w+))?)(?:=(\w+))?\}/', function ($match) use (& $args) {
+            $param_name=$match[1];
+            $param_type= $match[2] ?? 'url';
+            $param_default=$match[3]??'';
+            if (isset($args[$param_name])) {
+                if ($param_type==='int') {
+                    $val= intval($args[$param_name]);
+                }
+                $val=$args[$param_name];
+                unset($args[$param_name]);
+                return $val;
+            } else {
+                return $param_default;
+            }
+        }, preg_replace('/\[(.+?)\]/', '$1', $url));
+        if (count($args)) {
+            return Request::getInstance()->baseUrl(). trim($url, '/').'?'.http_build_query($args, 'v', '&', PHP_QUERY_RFC3986);
+        }
+        return Request::getInstance()->baseUrl(). trim($url, '/');
+    }
+
+    public function getPrefix()
+    {
+        $prefix= app()->getModulePrefix($this->module)??'';
+        $admin_prefix='';
+        if (is_array($prefix)) {
+            if (in_array(key($prefix), ['admin','simple'], true)) {
+                $admin_prefix=$prefix['admin'] ?? '';
+                $prefix=$prefix['simple'] ?? '';
+            } else {
+                $admin_prefix=count($prefix)?array_shift($prefix):'';
+                $prefix=count($prefix)?array_shift($prefix):'';
+            }
+        }
+        if ($this->role == self::ROLE_ADMIN) {
+            return conf('app.admin', '/admin').$admin_prefix;
+        } else {
+            return $prefix;
+        }
+    }
+
+    protected function buildMatch(string $url)
+    {
+        $types=&$this->types;
+        $urltype=self::$urlType;
+        // 转义字符
+        $url=preg_replace('/([\/\.\\\\\+\*\(\^\)\?\$\!\<\>\-])/', '\\\\$1', $url);
+        // 添加忽略
+        $url=preg_replace('/\[(\S+)\]/', '(?:$1)?', $url);
+        // 编译页面参数
+        $url=preg_replace_callback('/\{(?:(\w+)(?::(\w+))?)(?:=(\w+))?\}([?])?/', function ($match) use (&$types, $urltype) {
+            $size=isset($types)?count($types):0;
+            $param_name=$match[1]!==''?$match[1]:$size;
+            $param_type=  $match[2] ?? 'string';
+            $ignore=isset($match[4])?'?':'';
+            $types[$param_name]=$param_type;
+            if (isset($urltype[$param_type])) {
+                return '('.$urltype[$param_type].')'.$ignore;
+            } else {
+                return '(.+)'.$ignore;
+            }
+        }, $url);
+        return $url;
+    }
+
+    public function getModule()
+    {
+        return $this->module;
+    }
+
+    public function getTypes()
+    {
+        return $this->types;
+    }
+    
+    public function setAntiPrefix(bool $set=true)
+    {
+        $this->antiPrefix=$set;
+        return $this;
+    }
+
+    public function setMapping(string $mapping)
+    {
+        $this->mapping=$mapping;
+        return $this;
+    }
+
+    public function setUrl(string $url)
+    {
+        $this->url=$url;
+        return $this;
+    }
+
+    public static function createFromRouteArray(int $role, string $module, string $name, array $json)
+    {
+        $mapping= new self($name, $json['visit'], $json['class'].'->onRequest', $module, $json['method']??[], $role);
+        $mapping->antiPrefix=isset($json['anti-prefix'])?$json['anti-prefix']:false;
+        $mapping->hidden=isset($json['hidden'])?$json['hidden']:false;
+        $mapping->build();
+        return $mapping;
+    }
+}
