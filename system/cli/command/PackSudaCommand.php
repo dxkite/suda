214725724a2964b8f08929c0da9e49985c86bb89
DXkite <DXkite@163.com>
2018-09-12 17:30:23 +0800

    添加：配置检测

--- system/cli/command/PackSudaCommand.php
+++ system/cli/command/PackSudaCommand.php
@@ -8,10 +8,15 @@ class PackSudaCommand extends Command
 {
     public static function exec(array $argv)
     {
-        $path = $argv[1] ?? './suda-cli.phar';
-        $phar = new Phar($path,0,'suda.phar');
-        $phar->buildFromDirectory(SYSTEM_DIR);
-        $phar->setStub(Phar::createDefaultStub('suda-phar.php'));
-        $phar->compressFiles(Phar::GZ);
+        $isReadOnly = ini_get('phar.readonly');
+        if ($isReadOnly) {
+            echo 'please set phar.readonly to Off';
+        } else {
+            $path = $argv[1] ?? './suda-cli.phar';
+            $phar = new Phar($path, 0, 'suda.phar');
+            $phar->buildFromDirectory(SYSTEM_DIR);
+            $phar->setStub(Phar::createDefaultStub('suda-phar.php'));
+            $phar->compressFiles(Phar::GZ);
+        }
     }
 }
--- system/cli/command/ReleaseCommand.php
+++ system/cli/command/ReleaseCommand.php
@@ -8,10 +8,15 @@ class ReleaseCommand extends Command
 {
     public static function exec(array $argv)
     {
-        $path = './suda-cli_v'.SUDA_VERSION.'.phar';
-        $phar = new Phar($path,0,'suda.phar');
-        $phar->buildFromDirectory(SYSTEM_DIR);
-        $phar->setStub(Phar::createDefaultStub('suda-phar.php'));
-        $phar->compressFiles(Phar::GZ);
+        $isReadOnly = ini_get('phar.readonly');
+        if ($isReadOnly) {
+            echo 'please set phar.readonly to Off';
+        } else {
+            $path = './suda-cli_v'.SUDA_VERSION.'.phar';
+            $phar = new Phar($path, 0, 'suda.phar');
+            $phar->buildFromDirectory(SYSTEM_DIR);
+            $phar->setStub(Phar::createDefaultStub('suda-phar.php'));
+            $phar->compressFiles(Phar::GZ);
+        }
     }
 }
