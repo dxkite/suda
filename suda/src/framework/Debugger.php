56faee86de1f14e6de46b0dbb74be4c61320f95a
DXkite <DXkite@163.com>
2019-03-03 16:28:58 +0800

    调整：Logger

--- suda/src/framework/Debugger.php
+++ suda/src/framework/Debugger.php
@@ -1,15 +1,83 @@
 <?php
 namespace suda\framework;
 
+use suda\framework\Context;
+use suda\framework\Debugger;
 use suda\framework\debug\Debug;
-
-
+use suda\framework\runnable\Runnable;
+use suda\framework\filesystem\FileSystem;
+use suda\framework\debug\log\LoggerInterface;
+use suda\framework\debug\log\logger\FileLogger;
+use suda\framework\debug\log\logger\NullLogger;
 
 /**
  * 调试器
  */
 class Debugger extends Debug
 {
+    /**
+     * 创建调式工具
+     *
+     * @param Context $context
+     * @return Debugger
+     */
+    public static function create(Context $context): Debugger
+    {
+        $debugger = new Debugger;
+        $debugger->addAttribute('remote-ip', $context->get('request')->getRemoteAddr());
+        $debugger->addAttribute('debug', $context->get('config')->get('debug', false));
+        $debugger->addAttribute('request-uri', $context->get('request')->getUrl());
+        $debugger->addAttribute('request-method', $context->get('request')->getMethod());
+        $debugger->addAttribute('request-time', date('Y-m-d H:i:s', \constant('SUDA_START_TIME')));
+        $debugger->applyConfig([
+            'start-time' => \constant('SUDA_START_TIME'),
+            'start-memory' => \constant('SUDA_START_MEMORY'),
+        ]);
+        $debugger->setLogger(static::createLogger($context));
+        $debugger->logger->notice(PHP_EOL.'{request-time} {remote-ip} {request-method} {request-uri} debug={debug}', $debugger->getAttribute());
+        return $debugger;
+    }
+
+    /**
+     * 创建日志记录器
+     *
+     * @return LoggerInterface
+     */
+    protected static function createLogger(Context $context): LoggerInterface
+    {
+        $logger = (new Runnable($context->get('config')->get('app.logger-build', [__CLASS__, 'createDefaultLogger'])))->run();
+        if ($logger instanceof LoggerInterface) {
+            return $logger;
+        } else {
+            return new NullLogger;
+        }
+    }
+    
+    /**
+     * 创建默认记录器
+     *
+     * @return LoggerInterface
+     */
+    public static function createDefaultLogger(): LoggerInterface
+    {
+        $dataPath = SUDA_DATA.'/logs';
+        FileSystem::makes($dataPath);
+        if (\is_writable(dirname($dataPath))) {
+            FileSystem::makes($dataPath.'/zip');
+            FileSystem::makes($dataPath.'/dump');
+            return new FileLogger(
+            [
+                'log-level' => defined('SUDA_DEBUG_LEVEL') ? constant('SUDA_DEBUG_LEVEL') : 'debug',
+                'save-path' => $dataPath,
+                'save-zip-path' => $dataPath.'/zip',
+                'log-format' => '%message%',
+                'save-pack-path' => $dataPath.'/dump',
+            ]
+        );
+        }
+        return new NullLogger;
+    }
+    
     public function getDefaultConfig():array
     {
         return [
@@ -18,4 +86,4 @@ class Debugger extends Debug
             'start-memory' => 0,
         ];
     }
-} 
+}
--- suda/src/loader/web-run.php
+++ suda/src/loader/web-run.php
@@ -35,48 +35,7 @@ $context->setSingle('response', function () {
 });
 
 $context->setSingle('debug', function () use ($context) {
-    $logger = (new Runnable($context->get('config')->get(
-        'app.logger-build',
-        function () {
-            $dataPath = SUDA_DATA.'/logs';
-            FileSystem::makes($dataPath);
-            if (\is_writable(dirname($dataPath))) {
-                FileSystem::makes($dataPath.'/zip');
-                FileSystem::makes($dataPath.'/dump');
-                return new FileLogger(
-                [
-                    'log-level' => defined('SUDA_DEBUG_LEVEL') ? constant('SUDA_DEBUG_LEVEL') : 'debug',
-                    'save-path' => $dataPath,
-                    'save-zip-path' => $dataPath.'/zip',
-                    'log-format' => '%message%',
-                    'save-pack-path' => $dataPath.'/dump',
-                ]
-            );
-            }
-            return new NullLogger;
-        }
-    )))->run();
-
-    $debugger = new Debugger;
-    
-    $debugger->addAttribute('remote-ip', $context->get('request')->getRemoteAddr());
-    $debugger->addAttribute('debug', $context->get('config')->get('debug', false));
-    $debugger->addAttribute('request-uri', $context->get('request')->getUrl());
-    $debugger->addAttribute('request-method', $context->get('request')->getMethod());
-    
-    $debugger->addAttribute('request-time', date('Y-m-d H:i:s', \constant('SUDA_START_TIME')));
-
-    $debugger->applyConfig([
-        'start-time' => \constant('SUDA_START_TIME'),
-        'start-memory' => \constant('SUDA_START_MEMORY'),
-    ]);
-    if ($logger instanceof LoggerInterface) {
-        $debugger->setLogger($logger);
-    } else {
-        $debugger->setLogger(new NullLogger);
-    }
-    $logger->notice(PHP_EOL.'{request-time} {remote-ip} {request-method} {request-uri} debug={debug}', $debugger->getAttribute());
-    return $debugger;
+    return Debugger::create($context);
 });
 
 $context->get('debug')->notice('system booting');
