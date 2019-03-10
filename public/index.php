69c79c52ae982989ead824c6f90df60aedd66d3d
DXkite <DXkite@163.com>
2019-03-10 20:37:43 +0800

    修复：测试表

--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
 <?php
 
-use suda\framework\Server;
-
 define('SUDA_APP', __DIR__.'/../app');
 define('SUDA_DATA', __DIR__.'/../app/data');
 define('SUDA_SYSTEM', __DIR__.'/../suda');
--- suda/resource/app/modules/welcome/share/table/TestTable.php
+++ suda/resource/app/modules/welcome/share/table/TestTable.php
@@ -4,13 +4,14 @@ namespace suda\welcome\table;
 use suda\orm\DataSource;
 use suda\orm\TableStruct;
 use suda\application\database\Table;
+use suda\orm\connection\creator\MySQLTableCreator;
 
 class TestTable extends Table
 {
     public function __construct(DataSource $datasource)
     {
         parent::__construct('hello', $datasource);
-        $this->getSource()->write()->createTable($this->getStruct()->getFields());
+        (new MySQLTableCreator($this->getSource()->write(),$this->getStruct()->getFields()))->create();
     }
 
     public function onCreateStruct(TableStruct $struct):TableStruct
