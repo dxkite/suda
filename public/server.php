32b9bad2890bccb8caf82a58b84354b799eef287
dxkite <DXkite@163.com>
2019-07-11 20:59:42 +0800

    调整：Swoole入口

--- public/server.php
+++ public/server.php
@@ -7,53 +7,8 @@ define('SUDA_PUBLIC', __DIR__);
 define('SUDA_DEBUG', true);
 define('SUDA_DEBUG_LEVEL', 'info');
 define('SUDA_APP_MANIFEST', SUDA_APP . '/manifest');
-
-require_once SUDA_SYSTEM . '/loader/loader.php';
-
-
-use suda\framework\debug\log\logger\FileLogger;
-use suda\framework\loader\Loader;
-use suda\application\builder\ApplicationBuilder;
-use suda\swoole\Request;
-use suda\swoole\Response;
-use Swoole\Http\Server;
+define('SUDA_SWOOLE_IP', '127.0.0.1');
+define('SUDA_SWOOLE_PORT', 80);
 
 
-// 初始化系统加载器
-$loader = new Loader;
-$loader->register();
-$loader->addIncludePath(SUDA_SYSTEM . '/src', 'suda');
-
-$application = ApplicationBuilder::build($loader, SUDA_APP, SUDA_APP_MANIFEST, SUDA_DATA);
-
-$application->registerDebugger();
-
-$application->config()->set('copy-static-source', false);
-
-$logger = new FileLogger(
-    [
-        'log-level' => SUDA_DEBUG_LEVEL,
-        'save-path' => $application->getDataPath() . '/logs',
-        'save-zip-path' => $application->getDataPath() . '/logs/zip',
-        'log-format' => '%message%',
-        'save-pack-path' => $application->getDataPath() . '/logs/dump',
-    ]
-);
-
-
-$application->getDebug()->setLogger($logger);
-
-$http = new Server('127.0.0.1', 9501);
-
-$http->on('request', function ($request, $response) use ($application, $logger) {
-    $application->getDebug()->applyConfig([
-        'start-time' => microtime(true),
-        'start-memory' => memory_get_usage(),
-    ]);
-    $application->run(new Request($request), new Response($response));
-    $logger->write();
-});
-
-$http->start();
-
-
+require_once SUDA_SYSTEM.'/loader/swoole.php';
---
+++ suda/loader/swoole.php
@@ -0,0 +1,54 @@
+<?php
+require_once __DIR__ . '/loader.php';
+
+
+use suda\framework\debug\log\logger\FileLogger;
+use suda\framework\loader\Loader;
+use suda\application\builder\ApplicationBuilder;
+use suda\swoole\Request;
+use suda\swoole\Response;
+use Swoole\Http\Server;
+
+
+// 初始化系统加载器
+$loader = new Loader;
+$loader->register();
+$loader->addIncludePath(SUDA_SYSTEM . '/src', 'suda');
+
+// 创建应用
+$application = ApplicationBuilder::build($loader, SUDA_APP, SUDA_APP_MANIFEST, SUDA_DATA);
+
+// 注册Debug工具
+$application->registerDebugger();
+
+// 不复制资源
+$application->config()->set('copy-static-source', false);
+
+// 文件日志记录
+$logger = new FileLogger(
+    [
+        'log-level' => SUDA_DEBUG_LEVEL,
+        'save-path' => $application->getDataPath() . '/logs',
+        'save-zip-path' => $application->getDataPath() . '/logs/zip',
+        'log-format' => '%message%',
+        'save-pack-path' => $application->getDataPath() . '/logs/dump',
+    ]
+);
+
+
+$application->getDebug()->setLogger($logger);
+
+$http = new Server(SUDA_SWOOLE_IP, SUDA_SWOOLE_PORT);
+
+$http->on('request', function ($request, $response) use ($application, $logger) {
+    $application->getDebug()->applyConfig([
+        'start-time' => microtime(true),
+        'start-memory' => memory_get_usage(),
+    ]);
+    $application->run(new Request($request), new Response($response));
+    $logger->write();
+});
+
+$http->start();
+
+
